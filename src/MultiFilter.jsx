98461af8d405290117510bd6ad3890f7a61fe0ee
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-07-14 17:07:41 -0300

    Project Finished
    - All elements created and maped
    - All filter logic created
    - Styles updated

--- src/MultiFilter.jsx
+++ src/MultiFilter.jsx
@@ -1,5 +1,64 @@
+import { useEffect, useState } from "react"
+import { items } from "./Items"
+
 const MultiFilter = () => {
-	return <div>MultiFilter</div>
+	const [selectedFilters, setSelectedFilters] = useState([])
+	const [filteredItems, setFilteredItems] = useState(items)
+
+	let filters = ["Bags", "Watches", "Sports", "Sunglasses"]
+
+	const handleFilterButtonClick = (selectedCategory) => {
+		if (selectedFilters.includes(selectedCategory)) {
+			let filters = selectedFilters.filter((el) => el !== selectedCategory)
+			setSelectedFilters(filters)
+		} else {
+			setSelectedFilters([...selectedFilters, selectedCategory])
+		}
+	}
+
+	useEffect(() => {
+		filterItems()
+	}, [selectedFilters])
+
+	const filterItems = () => {
+		if (selectedFilters.length > 0) {
+			let tempItems = selectedFilters.map((selectedCategory) => {
+				let temp = items.filter((item) => item.category === selectedCategory)
+				return temp
+			})
+
+			setFilteredItems(tempItems.flat())
+		} else {
+			setFilteredItems([...items])
+		}
+	}
+
+	return (
+		<div>
+			<div className="buttons-container">
+				{filters.map((category, idx) => (
+					<button
+						key={`filters-${idx}`}
+						className={`button ${
+							selectedFilters?.includes(category) ? "active" : ""
+						}`}
+						onClick={() => handleFilterButtonClick(category)}
+					>
+						{category}
+					</button>
+				))}
+			</div>
+
+			<div className="items-container">
+				{filteredItems.map((item, idx) => (
+					<div key={`items-${idx}`} className="item">
+						<p>{item.name}</p>
+						<p className="category">{item.category}</p>
+					</div>
+				))}
+			</div>
+		</div>
+	)
 }
 
 export default MultiFilter
